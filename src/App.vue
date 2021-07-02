82ddd4b8ca12795c659f53688a834dbe80ef26b7
sorena <ha.amini74@gmail.com>
2021-07-02 10:07:24 +0430

    doc clean

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
   <div id="app">
-      <app-header></app-header>
-      <server-status></server-status>
-      <server-details></server-details>
-      <app-footer></app-footer>
+
   </div>
 </template>
 
 <script>
-    import AppHeader from "./components/shared/Header";
-    import ServerStatus from "./components/servers/ServerStatus";
-    import ServerDetails from "./components/servers/ServerDetails";
-    import AppFooter from "./components/shared/Footer";
+
     export default {
       name:'app',
         components:{
-            AppFooter,
-            ServerDetails,
-            ServerStatus,
-            AppHeader
         }
     }
 </script>
 
 <style scoped>
-    #app{
-        text-align: center;
-    }
+
 </style>
