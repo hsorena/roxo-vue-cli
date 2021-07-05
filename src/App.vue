9b06285b0e424f5e859b7e2d3344307b2f19e392
sorena <ha.amini74@gmail.com>
2021-07-05 21:45:38 +0430

    Clean doc

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,13 @@
 <template>
   <div id="app">
-      <div class="container">
-          <div class="row" style="margin: 30px">
-              <div class="col-xs-12 visible-lg-inline-block">
-                  <button class="btn btn-success" @click="selectedComponent = 'new' ">کامپوننت New</button>
-                  <button class="btn btn-danger" @click="selectedComponent = 'author' ">کامپوننت Author</button>
-                  <button class="btn btn-primary" @click="selectedComponent = 'quote' ">کامپوننت Quote</button>
-
-                  <keep-alive>
-                      <component :is="selectedComponent"></component>
-                  </keep-alive>
-
-<!--                                <quote>-->
-<!--                                    <h3 slot="title">جمله ای از بزرگان</h3>-->
-<!--                                    <p slot="content">{{ quote }}</p>-->
-<!--                                </quote>-->
-              </div>
-          </div>
-      </div>
-
 
   </div>
 </template>
 
 <script>
-    import Quote from "./components/Quote";
-    import New from "./components/New";
-    import Author from "./components/Author";
 
     export default {
-        data(){
-            return{
-                quote :"یه جمله از داستایوفسکی",
-                selectedComponent : 'quote'
-            }
-        },
-        components:{
-            'quote' : Quote,
-            'new' : New,
-            'author' : Author
-        }
+
     }
 </script>
 
