862dc4c27f7d616f0aba3a520aaa274d26c87526
sorena <ha.amini74@gmail.com>
2021-07-05 20:58:57 +0430

    Slots with name

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-
+    <quote>
+        <h3 slot="title">جمله ای از بزرگان</h3>
+        <p slot="content">{{ quote }}</p>
+    </quote>
   </div>
 </template>
 
 <script>
-    export default {
+    import Quote from "./components/Quote";
 
+    export default {
+        data(){
+            return{
+                quote :"یه جمله از داستایوفسکی"
+            }
+        },
+        components:{
+            'quote' : Quote
+        }
     }
 </script>
 
-<style>
+<style scoped>
 
 </style>
