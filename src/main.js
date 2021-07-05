6a90e0892d40a33b83fc8a61c172a03285bf1b7a
sorena <ha.amini74@gmail.com>
2021-07-05 20:43:27 +0430

    Clean doc

--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-export const eventBus = new Vue({
-  methods:{
-    changeAge(age){
-      this.$emit('ageWasEdited' , age)
-    }
-  }
-}); //state management
 
 new Vue({
   render: h => h(App),
