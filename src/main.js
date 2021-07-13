69ddb1484f77de04d17e1f883cf2ddd7f10d0b15
sorena <ha.amini74@gmail.com>
2021-07-13 15:24:45 +0430

    Working with http requests

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
+import vueResource from 'vue-resource'
 import App from './App.vue'
+
+Vue.use(vueResource)
+//افزودن url به روت http
+Vue.http.options.root = 'https://jsonplaceholder.typicode.com/'
+
+//پیش پردازش رو رکوئست ها
+// Vue.http.interceptors.push((request , next) => {
+//   if (request.method == 'POST'){
+//     request.method = 'PUT'
+//   }
+//   next(response => {
+//     response.json = () => {return {message:response.body}}
+//   })
+// })
+
+
 
 Vue.config.productionTip = false
 
