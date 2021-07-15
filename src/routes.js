dfcff75dd6af9c5651a19987b3500f08d606c623
sorena <ha.amini74@gmail.com>
2021-07-16 00:54:12 +0430

    Lazy loading

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,31 @@
 import Home from "@/components/Home";
-import User from "@/components/user/User";
-import UserStart from "@/components/user/UserStart";
-import UserDetail from "@/components/user/UserDetail";
-import UserEdit from "@/components/user/UserEdit";
+
 import Header from "@/components/Header";
 import NotFound from "@/components/NotFound";
+
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'] , ()=>{
+        resolve(require('./components/user/User'))
+    } , 'user')
+}
+
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'] , ()=>{
+        resolve(require('./components/user/UserStart'))
+    }, 'user')
+}
+
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'] , ()=>{
+        resolve(require('./components/user/UserDetail'))
+    }, 'user')
+}
+
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'] , ()=>{
+        resolve(require('./components/user/UserEdit'))
+    }, 'user')
+}
 
 export const routes =[
     {path : '' , name:'home' , components:{
